746b968e90d17988b1b827d172134286f2b57aad
04_Apiwat (Tee/ตี๋) <apiwat.pienwisetkaew@gmail.com>
2024-05-13 13:59:24 +0700

    ER-diagram

---
+++ er-diagram.js
@@ -0,0 +1,13 @@
+const author_M = {
+    authorID: 007,
+    name: "Tee",
+    address: {
+        houre_no : 685,
+        street: "khaosan road",
+        county: "Thailand",
+        postcode: "10600"
+    },
+    URL: "https://www.youtube.com/watch?v=NytJWLrHLuc&list=RDNytJWLrHLuc&start_radio=1"
+};
+
+console.log(author_M.address.street);
